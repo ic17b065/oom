dbaa7a2e28e707f59d490f6374d726cbddfad4be
ic17b065 <ic17b065@technikum-wien.at>
2018-03-31 11:12:51 +0200

    Task4

---
+++ tasks/Task4/Task4/Program-2018-03-30_14-17-29-436.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Task4
+{
+    interface IActuator
+    {
+        void Print();
+    }
+
+    class LinearDrive : IActuator
+    {
+        public void Print()
+        {
+            Console.WriteLine($"LinearDrive: { A_Name } Hub: {A_Hub}mm  Strom: { A_Current }A  Spannung: { A_Voltage }V  Leistung: { A_Power }W");
+        }
+
+        // private fields
+        private string Name;
+        private double Hub;
+        private double Current;
+        private double Voltage;
+
+        //Constructor
+        [JsonConstructor]
+        public LinearDrive(string newName, double newHub, double newCurrent, double newVoltage)
+        {
+            if (string.IsNullOrWhiteSpace(newName)) throw new ArgumentException("Name must not be empty.", nameof(newName));
+            if (newHub < 0) throw new ArgumentException("Hub must be higher than 0.", nameof(newHub));
+            if (newCurrent < 0) throw new ArgumentException("Current must be higher than 0.", nameof(newCurrent));
+            if ((newVoltage < 18) || (newVoltage > 30)) throw new ArgumentException("Voltage must be between 18 and 30.", nameof(newVoltage));
+
+            Name = newName;
+            Hub = newHub;
+            Current = newCurrent;
+            Voltage = newVoltage;
+        }
+
+        //public properties
+        public string A_Name => Name;
+        public double A_Hub => Hub;
+        public double A_Current => Current;
+        public double A_Voltage => Voltage;
+
+        public double A_Power => Current * Voltage;
+
+        //1 public method
+        /*public double GetPower(double current, double voltage)
+        {
+            if (current < 0) throw new ArgumentException("Current must be higher than 0.", nameof(current));
+            if ((voltage < 18) || (voltage > 30)) throw new ArgumentException("Voltage must be between 18 and 30.", nameof(voltage));
+
+            return current * voltage;    
+        }*/
+    }
+
+    class ChainDrive : IActuator
+    {
+        public void Print()
+        {
+            Console.WriteLine($"ChainDrive: { A_Name } Preis: {A_Prize}Euro");
+        }
+
+        // private fields
+        private string Name;
+        private double Prize;
+
+        //Constructor
+        [JsonConstructor]
+        public ChainDrive(string newName, double newPrize)
+        {
+            if (string.IsNullOrWhiteSpace(newName)) throw new ArgumentException("Name must not be empty.", nameof(newName));
+            if (newPrize < 0) throw new ArgumentException("Prize must be higher than 0.", nameof(newPrize));
+
+            Name = newName;
+            Prize = newPrize;
+
+        }
+
+        //public properties
+        public string A_Name => Name;
+        public double A_Prize => Prize;
+
+        //1 public method
+        public void SetPrize(double new_Prize)
+        {
+            if (new_Prize < 0)
+                throw new Exception("negativer Preis");
+            Prize = new_Prize;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                //creates instances (objects)
+                LinearDrive PLA = new LinearDrive("PLA", 500, 2.5, 24.0);
+                LinearDrive FTA = new LinearDrive("FTA", 200, 1.4, 24.0);
+                ChainDrive KSA = new ChainDrive("KSA", 433.20);
+
+                //calls method
+                KSA.SetPrize(477.42);
+
+                /*double iCurrent, iVoltage;
+                Console.WriteLine("\nCalculate new power for PLA:");
+                Console.WriteLine("Current: ");
+                string inCurrent = Console.ReadLine();
+                iCurrent = Convert.ToInt32(inCurrent);
+                Console.WriteLine("Voltage: ");
+                string inVoltage = Console.ReadLine();
+                iVoltage = Convert.ToInt32(inVoltage);
+                PLA.GetPower(iCurrent, iVoltage);*/
+
+                IActuator[] ActuatorArray = { PLA, FTA, new ChainDrive("KS2", 125.70), KSA };
+
+                foreach (IActuator Actuator in ActuatorArray)
+                {
+                    Actuator.Print();
+                }
+
+                //serialize ActuatorArray with all items, formatting and type included 
+                var settings = new JsonSerializerSettings() { Formatting = Formatting.Indented, TypeNameHandling = TypeNameHandling.Auto };
+
+                var json = JsonConvert.SerializeObject(ActuatorArray, settings);
+                Console.WriteLine(json);
+
+                //deserialize json
+                
+                var itemsfromjson = JsonConvert.DeserializeObject<IActuator[]>(json);
+
+                foreach (var Actuator in itemsfromjson)
+                { 
+                    Actuator.Print();
+                }
+
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Something happens {e.Message}");
+            }
+        }
+    }
+}
---
+++ tasks/Task4/Task4/Tests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task4
+{
+    [TestFixture]
+    public class AcutatorTests
+    {
+        //Linear Drives
+        [Test]
+        public void CanCreateLinearDrive()
+        {
+            var x = new LinearDrive("PLA", 400, 3.5, 24.0);
+
+            Assert.IsTrue(x.Name == "PLA");
+            Assert.IsTrue(x.Hub == 400);
+            Assert.IsTrue(x.Current == 3.5);
+            Assert.IsTrue(x.Voltage == 24.0);
+        }
+
+        [Test]
+        public void CannotCreateLinearDriveWithEmptyTitle1()
+        {
+            Assert.Catch(() =>
+            {
+            var x = new LinearDrive(null, 400, 3.5, 24.0);
+            });
+        }
+
+        [Test]
+        public void CannotCreateLinearDriveWithEmptyTitle2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new LinearDrive("", 400, 3.5, 24.0);
+            });
+        }
+
+        [Test]
+        public void CannotCreateLinearDriveWithNegativeHub()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new LinearDrive("PLA", -1, 3.5, 24.0);
+            });
+        }
+
+        [Test]
+        public void CannotCreateLinearDriveWithNegativeCurrent()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new LinearDrive("PLA", 400, -3.5, 24.0);
+            });
+        }
+
+        [Test]
+        public void CannotCreateLinearDriveWithVoltageLower18Higher30()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new LinearDrive("PLA", 400, 3.5, 18.0-30.0);
+            });
+        }
+
+        //Chain Drives
+        [Test]
+        public void CanCreateChainDrive()
+        {
+            var x = new ChainDrive("KSA", 433.42);
+
+            Assert.IsTrue(x.Name == "KSA");
+            Assert.IsTrue(x.Price == 433.42);
+        }
+
+        [Test]
+        public void CannotCreateChainDriveWithEmptyTitle1()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new ChainDrive(null, 433.42);
+            });
+        }
+
+        [Test]
+        public void CannotCreateChainDriveWithEmptyTitle2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new ChainDrive("", 433.42);
+            });
+        }
+        
+        [Test]
+        public void CannotCreateChainDriveWithNegativePrice()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new ChainDrive("KSA", -1);
+            });
+        }
+
+        [Test]
+        public void CanUpdateChainDriveWithPrice()
+        {
+            var x = new ChainDrive("KSA", 433.42);
+            x.SetPrice(475.90);
+
+            Assert.IsTrue(x.Price == 475.90);
+        }
+
+        [Test]
+        public void CannotUpdateChainDriveWithNegativePrice()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new ChainDrive("KSA", 433.42);
+                x.SetPrice(-56.73);
+            });
+        }
+    }
+}
